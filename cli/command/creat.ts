415f3f8d3745550d54f0b06b18be057625626516
daiyu <dai.yu@olaola.chat>
2024-06-26 16:47:47 +0800

    feat: 调整class

--- cli/command/creat.ts
+++ cli/command/creat.ts
@@ -6,58 +6,87 @@ import chalk from 'chalk';
 import { spinner, pathExistsReject } from '../../utils/index';
 import { custError } from '../../utils/error'
 
-const runCreat = async (params: { path: string }) => {
-
-  // tip
-  // spinner.start(`正在加载模板...\n`)
-  const tempUrl = path.resolve(__dirname, '../../template')
-  const tempList = await fs.readdir(tempUrl, { encoding: 'utf-8' })
-  // spinner.info(`模板加载完成\n`)
-
-  const answers = await inquirer.prompt([
-    {
-      type: 'rawlist',
-      name: 'projectName',
-      message: '请选择代码片段',
-      choices: [...tempList].map(v => (
-        {
-          name: v,
-          value: v,
-        }
-      ))
-    },
-    {
-      type: 'input',
-      name: 'fileName',
-      message: "请输入文件名",
-      validate(input, answers) {
-        console.log(input, answers)
-        if (!input) {
-          return chalk.yellow('文件名不能为空')
-        }
-        return true
-      }
-    }
-  ])
-
-  // tip 加载模板
-  spinner.start(`开始创建模板...\n`)
-
-  // check dir
-  const targetUrl = path.resolve(process.cwd(), params.path, answers.fileName)
-  await pathExistsReject(targetUrl)
-
-  // copy
-  const targetTempUrl = path.resolve(tempUrl, answers.projectName)
-  const isTargetTempUrl = await fse.pathExists(targetTempUrl)
-  if (!isTargetTempUrl) {
-    throw custError('')
-  }
-
-  await fse.copy(targetTempUrl, targetUrl)
-
-  // tip 创建完成
-  spinner.succeed(`创建完成...\n`)
+interface Params {
+  path: string
 }
 
-export default runCreat
+type pluginFn = (params: Params) => Promise<any>
+
+class RunCreat {
+
+  // 插件
+  static plugins: pluginFn[]
+
+  // 模板地址
+  tempUrl = path.resolve(__dirname, '../../template')
+
+  // 模板列表
+  tempList: string[] = []
+
+  constructor(private params: Params) { }
+
+  static add(fn: pluginFn) {
+    RunCreat.plugins.push(fn)
+  }
+
+  async start() {
+    this.tempList = await fs.readdir(this.tempUrl, { encoding: 'utf-8' })
+    const answers = await inquirer.prompt([
+      {
+        type: 'rawlist',
+        name: 'projectName',
+        message: '请选择代码片段',
+        choices: [...this.tempList].map(v => (
+          {
+            name: v,
+            value: v,
+          }
+        ))
+      },
+      {
+        type: 'input',
+        name: 'fileName',
+        message: "请输入文件名",
+        validate(input, answers) {
+          console.log(input, answers)
+          if (!input) {
+            return chalk.yellow('文件名不能为空')
+          }
+          return true
+        }
+      }
+    ])
+    // tip 加载模板
+    spinner.start(`开始创建模板...\n`)
+
+    // check dir
+    const targetUrl = path.resolve(process.cwd(), this.params.path, answers.fileName)
+    await pathExistsReject(targetUrl)
+
+    // copy
+    const copy = async () => {
+      const targetTempUrl = path.resolve(this.tempUrl, answers.projectName)
+      const isTargetTempUrl = await fse.pathExists(targetTempUrl)
+      if (!isTargetTempUrl) {
+        throw custError('')
+      }
+
+      await fse.copy(targetTempUrl, targetUrl)
+
+      // tip 创建完成
+      spinner.succeed(`创建完成...\n`)
+    }
+
+    // run plugin
+    for (let i = 0; i < RunCreat.plugins.length; i++) {
+      await RunCreat.plugins[i](this.params)
+    }
+
+    // copy
+    copy()
+
+  }
+
+}
+
+export default RunCreat
--- cli/index.ts
+++ cli/index.ts
@@ -3,14 +3,15 @@ import chalk from 'chalk'
 import { spinner } from '../utils/index';
 
 const program = new Command()
-import runCreat from './command/creat'
+import RunCreat from './command/creat'
 
 program
   .name('creat')
   .description('创建模板文件到指定目录')
   .requiredOption('-p, --path <string>', '路径')
   .action((options, command) => {
-    runCreat(options)
+    const runCreat = new RunCreat(options)
+    runCreat.start()
       .catch(e => {
         // console.error(JSON.stringify(e.stack))
         // console.error(chalk.bgRed(e.message))
