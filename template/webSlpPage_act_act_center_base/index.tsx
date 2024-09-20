3320983c5ce380b2037c76f306eeae049396062e
daiyu <dai.yu@olaola.chat>
2024-09-20 18:40:00 +0800

    feat: add temp

---
+++ template/webSlpPage_act_act_center_base/index.tsx
@@ -0,0 +1,8 @@
+import { useCallback, useEffect, useState, useMemo, useRef } from 'react'
+import style from './index.module.less'
+
+export default function Home() {
+  return (
+    <div className={style.home}>Home</div>
+  )
+}
